b621e69e238ec20189670c9da098442f7f69b16c
napor1 <napori130@gmail.com>
2025-01-16 23:35:20 +0800

    chore: add husky

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -19,7 +19,7 @@ const convert = (queryClient: QueryClient) => (m: any) => {
   };
 };
 
-export const createAppRouter = (queryClient: QueryClient) =>
+const createAppRouter = (queryClient: QueryClient) =>
   createBrowserRouter([
     {
       path: paths.root.path,
