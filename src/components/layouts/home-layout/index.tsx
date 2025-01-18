b9ccc7e5e3df54d11d4aec09bf6ee223dbaa8c29
napor1 <napori130@gmail.com>
2025-01-19 00:43:50 +0800

    init home layout

--- src/components/layouts/home-layout/index.tsx
+++ src/components/layouts/home-layout/index.tsx
@@ -1,16 +1,53 @@
 import { NavLink } from "react-router";
 import "./index.scss";
+import { Layout } from "antd";
 import { paths } from "@/config/paths";
+
+const { Header, Content, Footer } = Layout;
+
+const Banner = () => {
+  return (
+    <div className="home-banner">
+      <h1 className="home-banner-title">conduit</h1>
+      <p className="home-banner-subtitle">A place to share your knowledge.</p>
+    </div>
+  );
+};
+
+const Logo = () => {
+  return (
+    <NavLink className="logo" to={paths.home.getHref()}>
+      conduit
+    </NavLink>
+  );
+};
+
+const NavLinks = () => {
+  return (
+    <div className="nav-links-wrapper">
+      <NavLink className="nav-link" to={paths.home.getHref()}>
+        Home
+      </NavLink>
+      <NavLink className="nav-link" to={paths.auth.login.getHref()}>
+        Login
+      </NavLink>
+      <NavLink className="nav-link" to={paths.auth.register.getHref()}>
+        Register
+      </NavLink>
+    </div>
+  );
+};
 
 export function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="home-layout-wrapper">
-      <NavLink to={paths.home.getHref()}>Home</NavLink>
-      ||
-      <NavLink to={paths.auth.login.getHref()}>Login</NavLink>
-      ||
-      <NavLink to={paths.auth.register.getHref()}>Register</NavLink>
-      {children}
-    </div>
+    <Layout className="home-layout">
+      <Header className="home-header">
+        <Logo />
+        <NavLinks />
+      </Header>
+      <Banner />
+      <Content className="home-content">{children}</Content>
+      <Footer className="home-footer">Footer</Footer>
+    </Layout>
   );
 }
